1d293af334cdd00f2f6f4394192b4923a180c656
Gustavo Bretas <gustavobretas@gmail.com>
2022-04-10 21:08:59 -0400

    Accessing a relational database

--- DataAccess/main.go
+++ DataAccess/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -17,12 +16,33 @@ type Album struct {
 	Price  float64
 }
 
+func checkErrorFree(err error) bool {
+	if err != nil {
+		panic(err)
+	}
+	return true
+}
+
 func main() {
 	albums, err := albumsByArtist("John Coltrane")
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkErrorFree(err)
 	fmt.Printf("Albums found: %v\n", albums)
+
+	alb, err := albumByID(2)
+	checkErrorFree(err)
+	fmt.Printf("Album found: %v\n", alb)
+
+	albId, err := addAlbum(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+	checkErrorFree(err)
+	fmt.Printf("ID of added album: %v\n", albId)
+
+	if delAlbum(albId) {
+		fmt.Printf("Album Id: %v deleted", albId)
+	}
 }
 
 func PrepareSqlite() (db *sql.DB, err error) {
@@ -63,4 +83,54 @@ func albumsByArtist(name string) ([]Album, error) {
 	return albums, nil
 }
 
-// Continue https://go.dev/doc/tutorial/database-access
+// albumByID queries for the album with the specified ID.
+func albumByID(id int64) (Album, error) {
+	// An Album to hols data from the returned row.
+	var alb Album
+
+	db, err := PrepareSqlite()
+	checkErrorFree(err)
+
+	row := db.QueryRow("SELECT id, title, artist, price FROM album WHERE id = ?", id)
+	checkErrorFree(err)
+	err = row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price)
+	if err == sql.ErrNoRows {
+		return alb, fmt.Errorf("albumByID %d: no such album", id)
+	} else if err != nil {
+		return alb, fmt.Errorf("albumByID %d: %v", id, err)
+	}
+
+	return alb, nil
+
+}
+
+// addAlbum adds specified album to the database.
+// returning the album ID of the new entry
+func addAlbum(alb Album) (int64, error) {
+	db, err := PrepareSqlite()
+	checkErrorFree(err)
+
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	checkErrorFree(err)
+
+	id, err := result.LastInsertId()
+	checkErrorFree(err)
+
+	return id, nil
+}
+
+// delAlbum delete specified album to the database
+// returning the object deleted.
+func delAlbum(id int64) bool {
+	db, err := PrepareSqlite()
+	checkErrorFree(err)
+
+	result, err := db.Exec("DELETE FROM album where id = ?", id)
+	checkErrorFree(err)
+
+	rows, err := result.RowsAffected()
+	checkErrorFree(err)
+
+	return rows > 0
+
+}
