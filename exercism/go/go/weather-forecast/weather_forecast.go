481992fe2f5572dab3bd0615f901bf4fe47c2187
Gustavo Bretas <gustavobretas@gmail.com>
2022-09-02 10:02:36 -0400

    exercise Weather Forecast (Done)

--- exercism/go/go/weather-forecast/weather_forecast.go
+++ exercism/go/go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides resources to consult the Forecast.
 package weather
 
+// CurrentCondition stores de Current Forecast Condition to be used at Forecast Function.
 var CurrentCondition string
+
+// CurrentLocation stores de Current Locatrion to be used at Forecast Function.
 var CurrentLocation string
 
+// Forecast resolve the Forecast Condition by city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
