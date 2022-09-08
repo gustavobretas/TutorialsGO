bae42eb2eb8815b8b857af7fc66208754e09821e
Gustavo Bretas <gustavobretas@gmail.com>
2022-09-08 11:17:56 -0400

    exercise Lasagna Master (Done)

--- exercism/go/lasagna-master/lasagna_master.go
+++ exercism/go/lasagna-master/lasagna_master.go
@@ -1,9 +1,34 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+// PreparationTime estimates the preparation time based on the number of layers
+func PreparationTime(layers []string, avgPrepTime int) int {
+	if avgPrepTime == 0 {
+		avgPrepTime = 2
+	}
+	return len(layers) * avgPrepTime
+}
 
-// TODO: define the 'Quantities()' function
+// Quantities calculates the amount of Noodles and Sauce need to prepare the lasagna
+func Quantities(layers []string) (noodles int, sauce float64) {
+	for _, item := range layers {
+		if item == "noodles" {
+			noodles += 50
+		} else if item == "sauce" {
+			sauce += 0.2
+		}
+	}
+	return
+}
 
-// TODO: define the 'AddSecretIngredient()' function
+// AddSecretIngredient get a last ingredient from your friends list to your list
+func AddSecretIngredient(friendsList []string, myList []string) {
+	myList[len(myList)-1] = friendsList[len(friendsList)-1]
+}
 
-// TODO: define the 'ScaleRecipe()' function
+// ScaleRecipe scales the portion of ingredients
+func ScaleRecipe(sliceAmountOfTwo []float64, scaledQuantities int) (scaled []float64) {
+	for _, item := range sliceAmountOfTwo {
+		scaled = append(scaled, item*(float64(scaledQuantities)/2))
+	}
+	return
+}
