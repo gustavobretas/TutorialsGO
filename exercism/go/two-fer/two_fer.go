1c6078157473b67daca05ebd7dd95520c3ae7069
Gustavo Bretas <gustavobretas@gmail.com>
2022-10-01 21:51:02 -0400

    exercise Two Fer (Refactored)

--- exercism/go/two-fer/two_fer.go
+++ exercism/go/two-fer/two_fer.go
@@ -1,13 +1,8 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package twofer
 
 import "fmt"
 
-// ShareWith should have a comment documenting it.
+// ShareWith is about share with a given name
 func ShareWith(name string) string {
 	if len(name) == 0 {
 		name = "you"
