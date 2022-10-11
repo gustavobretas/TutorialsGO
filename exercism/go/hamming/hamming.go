ad48a8cc90afa4d7bb11dfdf3a6ac10924c04f53
Gustavo Bretas <gustavobretas@gmail.com>
2022-10-10 22:16:35 -0400

    exercise Hamming (Done)

--- exercism/go/hamming/hamming.go
+++ exercism/go/hamming/hamming.go
@@ -1,5 +1,16 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return -1, errors.New("sequence different length")
+	}
+	diff := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			diff++
+		}
+	}
+	return diff, nil
 }
