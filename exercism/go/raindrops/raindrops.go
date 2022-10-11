f841b21cd9faf91964afd72dceea0f7265186835
Gustavo Bretas <gustavobretas@gmail.com>
2022-10-10 22:39:30 -0400

    exercise Raindrops (Done)

--- exercism/go/raindrops/raindrops.go
+++ exercism/go/raindrops/raindrops.go
@@ -1,5 +1,19 @@
 package raindrops
 
-func Convert(number int) string {
-	panic("Please implement the Convert function")
+import "strconv"
+
+func Convert(number int) (res string) {
+	if number%3 == 0 {
+		res = res + "Pling"
+	}
+	if number%5 == 0 {
+		res = res + "Plang"
+	}
+	if number%7 == 0 {
+		res = res + "Plong"
+	}
+	if res == "" {
+		res = strconv.FormatInt(int64(number), 10)
+	}
+	return
 }
