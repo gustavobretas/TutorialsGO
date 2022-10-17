2c470bf658cc84787f82600f219629f13e438091
Gustavo Bretas <gustavobretas@gmail.com>
2022-10-17 00:00:22 -0400

    exercise Grains (Done)
    Tests work, but the is some think wrong with the function Total

--- exercism/go/grains/grains.go
+++ exercism/go/grains/grains.go
@@ -1,9 +1,21 @@
 package grains
 
+import (
+	"errors"
+	"math"
+)
+
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	if number <= 0 {
+		return 0, errors.New("negative square")
+	} else if number > 64 {
+		return 0, errors.New("greater than 64")
+	}
+
+	return uint64(math.Pow(float64(2), float64(number-1))), nil
 }
 
 func Total() uint64 {
-	panic("Please implement the Total function")
+
+	return uint64(math.Pow(float64(2), float64(64)) - 1)
 }
